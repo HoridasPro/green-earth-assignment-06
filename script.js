4e56781453da35603a6dcc0c109b62b5e5f1b7d1
HoridasPro <horidassarker6@gmail.com>
2025-09-08 17:29:22 +0600

    added to card section, about the campaign section and the our impact section

---
+++ script.js
@@ -0,0 +1,29 @@
+const cardSection = () => {
+  // const url = `https://openapi.programming-hero.com/api/plants`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => wordSection(data.plants));
+};
+const wordSection = (words) => {
+  const wordContainer = document.getElementById("word_container");
+  wordContainer.innerHTML = "";
+  for (const word of words) {
+    const createDivs = document.createElement("word_container");
+    createDivs.innerHTML = `
+          <div class=" p-3 bg-white shadow-5xl">
+           <figure><img class="w-[250px] h-[250px] rounded-xl" src="${word.image}" alt="" /></figure>
+            <h3 class="font-bold">${word.name}</h3>
+            <p>
+              A fast-growing tropical tree that produces delicious, <br />
+              juicy mangoes during summer. Its dense green
+            </p>
+            <div class="flex justify-between">
+              <button>Fruit Tree</button>
+              <span>৳500</span>
+            </div>
+          </div>
+      `;
+    wordContainer.appendChild(createDivs);
+  }
+};
+cardSection();
